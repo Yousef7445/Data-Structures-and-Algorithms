ca9471cd6291fb0dce5e38b2d9e856c941d6221a
George Yousef <22310090@scme.edu.ps>
2024-11-05 09:10:04 +0200

    Update yousef.py

--- 03. Data Structures/Arrays/yousef.py
+++ 03. Data Structures/Arrays/yousef.py
@@ -13,19 +13,25 @@
 #print(list3)
 #list4 = [x for x in list3 if x <1.5]
 #print(list4)
-list1 = ["green","red","blue"]
-list2 = ["red","blue","green"]
-print(list1==list2)
+#list1 = ["green","red","blue"]
+#list2 = ["red","blue","green"]
+#print(list1==list2)
 
-print(list1 != list2)
+#print(list1 != list2)
 
-print(list1 >= list2)
+#print(list1 >= list2)
 
-print (list1 > list2)
+#print (list1 > list2)
 
-print( list1 < list2)
+#print( list1 < list2)
 
-items = "Welcome to the SCME".split()
-print(items)
-items2 = "34#13#78#45".split("#")
-print(items2)
+#items = "Welcome to the SCME".split()
+#print(items)
+#items2 = "34#13#78#45".split("#")
+#print(items2)
+x = int(input("Add Num: "))
+list1 = [2,4,6]
+list2 = [1,3,7]
+if x == 1:
+    list3 = list1 + list2
+    print(list3)
