c02d8c27f8a5e17117675b41c0094c65d1843c42
George Yousef <22310090@scme.edu.ps>
2024-11-05 09:01:10 +0200

    Update yousef.py

--- 03. Data Structures/Arrays/yousef.py
+++ 03. Data Structures/Arrays/yousef.py
@@ -1,8 +1,31 @@
-list1 = [2,3,5,2,33,21]
+#list1 = [2,3,5,2,33,21]
 
-i = 0
+#i = 0
 
-while i < len(list1):
-    print(list1[i])
-    i += 1
-    
+#while i < len(list1):
+    #print(list1[i])
+    #i += 1
+
+#list2 =[x for x in range(0,5)]
+#print(list2)
+#list2 = [0,1,2,3,4]
+#list3 =[0.5 * x for x in list2]
+#print(list3)
+#list4 = [x for x in list3 if x <1.5]
+#print(list4)
+list1 = ["green","red","blue"]
+list2 = ["red","blue","green"]
+print(list1==list2)
+
+print(list1 != list2)
+
+print(list1 >= list2)
+
+print (list1 > list2)
+
+print( list1 < list2)
+
+items = "Welcome to the SCME".split()
+print(items)
+items2 = "34#13#78#45".split("#")
+print(items2)
