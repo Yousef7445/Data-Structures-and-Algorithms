327f26944d3832e399b273611953896de3479017
George Yousef <22310090@scme.edu.ps>
2024-12-03 08:27:30 +0200

    Create import random.py

---
+++ 03. Data Structures/Arrays/import random.py
@@ -0,0 +1,16 @@
+def Exchange( money, type):
+    USD = 3.7
+    JOD = 5.3
+    total = 0
+    if type == 'A' or type == 'a':
+        total = money*USD
+    elif type == 'B' or type == 'b':
+        total = money*JOD
+    elif print("error"):
+        exit()
+        return(total)
+print("enter A or B :")
+    
+    
+
+    
