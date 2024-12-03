2a921365a08ccb276469b504f3c7f3e8b5fe6399
George Yousef <22310090@scme.edu.ps>
2024-12-03 09:02:40 +0200

    .

--- 03. Data Structures/Arrays/yousef.py
+++ 03. Data Structures/Arrays/yousef.py
@@ -56,35 +56,46 @@
 # lst.sort()
 
 # print((lst))
-import random
-total = 0
-matrix = []
-numOFrows = int(input("Enter the number of rows: ")) 
-numOFcol = int(input("Enter the number of columns: "))  
+# import random
+# total = 0
+# matrix = []
+# numOFrows = int(input("Enter the number of rows: ")) 
+# numOFcol = int(input("Enter the number of columns: "))  
 
 
-for row in range(numOFrows):
-    matrix.append([])  
-    for col in range(numOFcol):
-        matrix[row].append(random.randrange(0, 100))
-        total += matrix[row][col]
+# for row in range(numOFrows):
+#     matrix.append([])  
+#     for col in range(numOFcol):
+#         matrix[row].append(random.randrange(0, 100))
+#         total += matrix[row][col]
           
 
 
-print(matrix, "Total is :"+str(total))
-print("Sum for column " + str(col) + "is :"+ str(total))
+# print(matrix, "Total is :"+str(total))
+# print("Sum for column " + str(col) + "is :"+ str(total))
 
-t1 = ()
-t2 =(1,3,5)
-print(t2)
-t3 = tuple([2 * x for x in range(1, 5)])
-print(t3)
-t4 = tuple("abac")
-print(t4)
-t5 = t2 + t3 + t4 
-print(t5)
+# t1 = ()
+# t2 =(1,3,5)
+# print(t2)
+# t3 = tuple([2 * x for x in range(1, 5)])
+# print(t3)
+# t4 = tuple("abac")
+# print(t4)
+# t5 = t2 + t3 + t4 
+# print(t5)
 
 
-for x in range(0,100):
-    x = "hello"
-    print(x)
+# # for x in range(0,100):
+# #     x = "hello"
+# #     print(x)
+s1 = set()
+s2 = {1, 3, 5}
+print(s2)
+s3 =  set([2 * x for x in range(1, 10)])
+print(sum(s3))
+s4 = set("abac")
+print(s4)
+s5 = s2 and s3 and s4 
+print(s5)
+
+
